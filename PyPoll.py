500432b95abd3fee7f66f0d36cfd52ac19731a37
Dwayne Bost <ncsu1289@gmail.com>
2017-10-03 18:10:21 -0400

    Upload all documents under Homework_3

---
+++ PyPoll.py
@@ -0,0 +1,18 @@
+'''You will be given two sets of poll data (election_data_1.csv and election_data_2.csv). Each dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+The total number of votes cast
+A complete list of candidates who received votes
+The percentage of votes each candidate won
+The total number of votes each candidate won
+The winner of the election based on popular vote.
+As an example, your analysis should look similar to the one below:
+Election Results
+-------------------------
+Total Votes: 620100
+-------------------------
+Rogers: 36.0% (223236)
+Gomez: 54.0% (334854)
+Brentwood: 4.0% (24804)
+Higgins: 6.0% (37206)
+-------------------------
+Winner: Gomez
+-------------------------'''
